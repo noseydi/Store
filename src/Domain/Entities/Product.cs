3f17798a2696d56f024734c2e1d6b2ccb00c8602
nura <nura@DESKTOP-8HSSN9E>
2025-01-28 13:07:05 +0330

    فصل 4 add base entity

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,19 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Product
+    public class Product : BaseAuditableEntity , ICommands
     {
-        public int Id { get; set; }
-        public string Name { get; set;  }
+        public string Title { get; set;  }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+
+        public int ProductTypeId { get; set; }
+        public int ProductBeandId { get; set; }
+
+
+        public string Description { get; set ; }
+        public bool IsActive { get  ; set  ; }
+        public bool IsDelete { get  ; set  ; }
+        public string Summary { get  ; set  ; }
     }
 }
---
+++ src/Domain/Entities/ProductType.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductType : BaseAuditableEntity, ICommands
+    {
+        public string Description { get  ; set  ; }
+        public bool IsActive { get  ; set  ; }
+        public bool IsDelete { get  ; set  ; }
+        public string Summary { get  ; set  ; }
+    }
+}
