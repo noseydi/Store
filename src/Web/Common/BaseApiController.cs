b24cb663def10a3c63bf6c16796cce18597c3447
nura <nura@DESKTOP-8HSSN9E>
2025-01-28 04:41:01 +0330

    declare realation

---
+++ src/Web/Common/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Common
+{
+    [ApiController]
+    [Route(template:"api/controller")]
+    public class BaseApiController : Controller
+    {
+      
+    }
+}
