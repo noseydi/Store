27e96f657a5b8041b0be9f58d3843f8ba6527418
nura <nura@DESKTOP-8HSSN9E>
2025-01-28 11:06:34 +0330

    add product with context

---
+++ src/Web/ConfigureService.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
+
+namespace Web
+{
+    public  static class ConfigureService
+    {
+        public static IServiceCollection AddWebServiceCollection (this WebApplicationBuilder builer )
+        {
+            builer.Services.AddDbContext<ApplicationDbContext>( option =>
+            {
+                option.UseSqlServer(builer.Configuration.GetConnectionString("DefaultConnection"));
+            }
+            );
+            return builer.Services ;
+        }
+    }
+}
