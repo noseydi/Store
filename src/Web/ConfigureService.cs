dcd96658ab77407f6af7312dfd3b16d94330037e
nura <nura@DESKTOP-8HSSN9E>
2025-02-01 00:19:26 +0330

    2-1 Generic Repository

--- src/Web/ConfigureService.cs
+++ src/Web/ConfigureService.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Persistence.Configurations;
+using Infrastructure.Persistence.SeedData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System.Runtime.CompilerServices;
@@ -7,9 +8,47 @@ namespace Web
 {
     public  static class ConfigureService
     {
-        public static IServiceCollection AddWebServiceCollection (this WebApplicationBuilder builer )
+        public static IServiceCollection AddWebServiceCollection (this WebApplicationBuilder builder )
         {
-            return builer.Services;
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            return builder.Services;
+        }
+        public static async Task<IApplicationBuilder> AddWebAppServiceAsync(this WebApplication app)
+        {
+            var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var logerfactory = services.GetRequiredService<ILoggerFactory>();
+            var context = services.GetRequiredService<ApplicationDbContext>();
+
+            try
+            {
+                await context.Database.MigrateAsync();
+                await GenerateFakeData.SeedDataAsync(context, logerfactory);
+
+            }
+            catch (Exception e)
+            {
+                var logger = logerfactory.CreateLogger<Program>();
+                logger.LogError(e, "error exception for migration");
+            }
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            await app.RunAsync();
+            return app;
         }
     }
 }
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -8,40 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddInstructionServices(builder.Configuration);
 builder.AddWebServiceCollection();
 
 var app = builder.Build();
-var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-var logerfactory = services.GetRequiredService<ILoggerFactory>();
-var context = services.GetRequiredService<ApplicationDbContext>();
-
-try
-{
-    await context.Database.MigrateAsync();
-    await GenerateFakeData.SeedDataAsync(context, logerfactory);
-
-}
-catch (Exception e)
-{
-    var logger = logerfactory.CreateLogger<Program>();
-    logger.LogError(e, "error exception for migration");
-}
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.AddWebAppServiceAsync().ConfigureAwait(false);
